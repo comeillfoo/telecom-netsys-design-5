d087e88637c52433b2663582623f033bf4d7bf2b
come_ill_foo <rayveleaveme34@inbox.ru>
2024-04-07 16:36:09 +0100

    feat: make generators selectable

--- simple-global-routing.cc
+++ simple-global-routing.cc
@@ -35,6 +35,8 @@
 #include "ns3/network-module.h"
 #include "ns3/point-to-point-module.h"
 
+#include "ns3/bulk-send-helper.h"
+
 #include <cassert>
 #include <fstream>
 #include <iostream>
@@ -53,9 +55,9 @@ main(int argc, char* argv[])
   LogComponentEnable ("SimpleGlobalRoutingExample", LOG_LEVEL_INFO);
 #endif
 
-    // Set up some default values for the simulation.  Use the
+    // Set up some default values for the OnOffApplications' simulation.  Use the
     Config::SetDefault("ns3::OnOffApplication::PacketSize", UintegerValue(210));
-    Config::SetDefault("ns3::OnOffApplication::DataRate", StringValue("448kb/s"));
+    Config::SetDefault("ns3::OnOffApplication::DataRate", StringValue("1Mb/s"));
 
     // DefaultValue::Bind ("DropTailQueue::m_maxPackets", 30);
 
@@ -64,16 +66,17 @@ main(int argc, char* argv[])
     CommandLine cmd(__FILE__);
     bool enableFlowMonitor = false;
     cmd.AddValue("EnableMonitor", "Enable Flow Monitor", enableFlowMonitor);
+
+    bool rttOrBandwidthGenerator = false;
+    cmd.AddValue("RTTorBandwidthGenerator", "Choose between measuring RTT or Bandwidth",
+		 rttOrBandwidthGenerator);
     cmd.Parse(argc, argv);
 
-    // Here, we will explicitly create four nodes.  In more sophisticated
+    // Here, we will explicitly create two nodes.  In more sophisticated
     // topologies, we could configure a node factory.
     NS_LOG_INFO("Create nodes.");
     NodeContainer c;
     c.Create(2);
-    // NodeContainer n0n2 = NodeContainer(c.Get(0), c.Get(2));
-    // NodeContainer n1n2 = NodeContainer(c.Get(1), c.Get(2));
-    // NodeContainer n3n2 = NodeContainer(c.Get(3), c.Get(2));
 
     InternetStackHelper internet;
     internet.Install(c);
@@ -85,60 +88,60 @@ main(int argc, char* argv[])
     p2p.SetChannelAttribute("Delay", StringValue("2ms"));
     NetDeviceContainer d0d1 = p2p.Install(c);
 
-    // NetDeviceContainer d1d2 = p2p.Install(n1n2);
-
-    // p2p.SetDeviceAttribute("DataRate", StringValue("1500kbps"));
-    // p2p.SetChannelAttribute("Delay", StringValue("10ms"));
-    // NetDeviceContainer d3d2 = p2p.Install(n3n2);
-
     // Later, we add IP addresses.
     NS_LOG_INFO("Assign IP Addresses.");
     Ipv4AddressHelper ipv4;
     ipv4.SetBase("192.168.211.0", "255.255.255.0");
     Ipv4InterfaceContainer i0i1 = ipv4.Assign(d0d1);
 
-    // ipv4.SetBase("10.1.2.0", "255.255.255.0");
-    // Ipv4InterfaceContainer i1i2 = ipv4.Assign(d1d2);
-
-    // ipv4.SetBase("10.1.3.0", "255.255.255.0");
-    // Ipv4InterfaceContainer i3i2 = ipv4.Assign(d3d2);
-
     // Create router nodes, initialize routing database and set up the routing
     // tables in the nodes.
     Ipv4GlobalRoutingHelper::PopulateRoutingTables();
 
-    // Create the OnOff application to send UDP datagrams of size
-    // 210 bytes at a rate of 448 Kb/s
-    NS_LOG_INFO("Create Applications.");
-    uint16_t port = 9; // Discard port (RFC 863)
-    OnOffHelper onoff("ns3::UdpSocketFactory",
-                      Address(InetSocketAddress(i0i1.GetAddress(1), port)));
-    onoff.SetConstantRate(DataRate("448kb/s"));
-    ApplicationContainer apps = onoff.Install(c.Get(0));
-    apps.Start(Seconds(1.0));
-    apps.Stop(Seconds(10.0));
 
-    // Create a packet sink to receive these packets
-    PacketSinkHelper sink("ns3::UdpSocketFactory",
-                          Address(InetSocketAddress(Ipv4Address::GetAny(), port)));
-    apps = sink.Install(c.Get(1));
-    apps.Start(Seconds(1.0));
-    apps.Stop(Seconds(10.0));
+    const uint16_t port = 5201;
+    if (rttOrBandwidthGenerator) {
+	// Generate slowed TCP for RTT
+    	// Create the OnOff application to send TCP segments at a rate of 1Mb/s
+    	NS_LOG_INFO("Create Applications.");
+    	OnOffHelper onoff("ns3::TcpSocketFactory",
+        	          Address(InetSocketAddress(i0i1.GetAddress(1), port)));
+    	onoff.SetConstantRate(DataRate("1Mb/s"));
+    	ApplicationContainer apps = onoff.Install(c.Get(0));
+    	apps.Start(Seconds(1.0));
+    	apps.Stop(Seconds(10.0));
 
-    // Create a similar flow from n3 to n1, starting at time 1.1 seconds
-    // onoff.SetAttribute("Remote", AddressValue(InetSocketAddress(i1i2.GetAddress(0), port)));
-    // apps = onoff.Install(c.Get(3));
-    // apps.Start(Seconds(1.1));
-    // apps.Stop(Seconds(10.0));
+    	// Create a packet sink to receive these packets
+    	PacketSinkHelper sink("ns3::TcpSocketFactory",
+        		      Address(InetSocketAddress(Ipv4Address::GetAny(), port)));
+    	apps = sink.Install(c.Get(1));
+    	apps.Start(Seconds(1.0));
+    	apps.Stop(Seconds(10.0));
 
-    // Create a packet sink to receive these packets
-    // apps = sink.Install(c.Get(1));
-    // apps.Start(Seconds(1.1));
-    // apps.Stop(Seconds(10.0));
+    } else {
+	// Generate TCP/UDP for maximum bandwidth
+	BulkSendHelper bulksend("ns3::UdpSocketFactory",
+				Address(InetSocketAddress(i0i1.GetAddress(1), port)));
+	// Set the amount of data to send in bytes. Zero is unlimited
+	const uint32_t maxBytes = 0;
+	bulksend.SetAttribute("MaxBytes", UintegerValue(maxBytes));
+
+	ApplicationContainer apps = bulksend.Install(c.Get(0));
+	apps.Start(Seconds(1.0));
+	apps.Stop(Seconds(10.0));
+
+	PacketSinkHelper sink("ns3::UdpSocketFactory",
+			      Address(InetSocketAddress(Ipv4Address::GetAny(), port)));
+	apps = sink.Install(c.Get(1));
+	apps.Start(Seconds(1.0));
+	apps.Stop(Seconds(10.0));
+    }
+    
+    const char* pcap_files_names = rttOrBandwidthGenerator ? "rtt-simple-global-routing" : "speed-simple-global-routing";
 
     AsciiTraceHelper ascii;
     p2p.EnableAsciiAll(ascii.CreateFileStream("simple-global-routing.tr"));
-    p2p.EnablePcapAll("simple-global-routing");
+    p2p.EnablePcapAll(pcap_files_names);
 
     // Flow Monitor
     FlowMonitorHelper flowmonHelper;
